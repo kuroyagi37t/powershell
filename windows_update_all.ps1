e015e20e381761d33e94b100171b098727fd9292
kuroyagi37t <kuroyagi@37t.net>
2016-02-05 17:05:34 +0900

    Update windows_update_all.ps1

--- windows_update_all.ps1
+++ windows_update_all.ps1
@@ -5,7 +5,7 @@ $AutoRestartIfPending = $true
 $LogPath = Split-Path $MyInvocation.MyCommand.Path  -Parent
 $LogFile = "WindowsUpdateShell.log"
  
-function Write-WindowsUpdateLog ($Message)
+function Write-Log ($Message)
 {
     $Output = "{0} {1}" -f (Get-Date), $Message
     Write-Output $Output
@@ -14,20 +14,20 @@ function Write-WindowsUpdateLog ($Message)
 
 try
 {
-    Write-WindowsUpdateLog "Checking for available updates Start"
+    Write-Log "Checking for available updates Start"
     #Checking for available updates 
     $updateSession = new-object -com "Microsoft.Update.Session"
     $criteria="IsInstalled=0 and Type='Software' and BrowseOnly = 0" 
     $updates=$updateSession.CreateupdateSearcher().Search($criteria).Updates 
     $downloader = $updateSession.CreateUpdateDownloader()           
     $downloader.Updates = $Updates
-    Write-WindowsUpdateLog "Checking for available updates End"
+    Write-Log "Checking for available updates End"
  
   
     #If no updates available, do nothing 
     if ($downloader.Updates.Count -ne "0") { 
         #If updates are available, download and install 
-        Write-WindowsUpdateLog "Downloading $($downloader.Updates.count) updates"  
+        Write-Log "Downloading $($downloader.Updates.count) updates"  
   
         $resultcode= @{0="Not Started"; 1="In Progress"; 2="Succeeded"; 3="Succeeded With Errors"; 4="Failed" ; 5="Aborted" } 
         $Result= $downloader.Download() 
@@ -36,18 +36,18 @@ try
             $updatesToInstall = New-object -com "Microsoft.Update.UpdateColl"
             foreach ($Update in $Updates | where {$_.isdownloaded} ){
                 $updatesToInstall.Add($Update) | out-null
-                Write-WindowsUpdateLog $Update.Title
+                Write-Log $Update.Title
             }
   
             $installer = $updateSession.CreateUpdateInstaller()        
             $installer.Updates = $updatesToInstall
-            Write-WindowsUpdateLog "Update Install Start"     
+            Write-Log "Update Install Start"     
             $installationResult = $installer.Install()
-            Write-WindowsUpdateLog "Update Install End"     
+            Write-Log "Update Install End"     
    
             #Reboot if autorestart is enabled and one or more updates are requiring a reboot 
             if ($autoRestart -and $installationResult.rebootRequired) { 
-                Write-WindowsUpdateLog "Status is Reboot Required. Reboot Start"
+                Write-Log "Status is Reboot Required. Reboot Start"
                 Restart-Computer -Force
                 Exit
             }        
@@ -55,13 +55,13 @@ try
     }
     if (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"){ 
         if ($AutoRestartIfPending) {
-            Write-WindowsUpdateLog "Status is Auto Restart Pending. Reboot Start"
+            Write-Log "Status is Auto Restart Pending. Reboot Start"
             Restart-Computer -Force
         }        
     }
 }
 catch
 {
-    Write-WindowsUpdateLog $_
-    Write-WindowsUpdateLog "Windows Update Error"
+    Write-Log $_
+    Write-Log "Windows Update Error"
 }
